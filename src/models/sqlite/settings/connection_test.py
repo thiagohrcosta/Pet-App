4034283cf45c917aee94483a9ed7f32e7448e2fe
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-04-03 20:07:06 -0300

    Config

--- src/models/sqlite/settings/connection_test.py
+++ src/models/sqlite/settings/connection_test.py
@@ -1,5 +1,8 @@
+import pytest
+from sqlalchemy.engine import Engine
 from src.models.sqlite.settings.connection import db_connection_handler
 
+@pytest.mark.skip(reason='database interaction')
 def test_connect_to_db():
   assert db_connection_handler.get_engine() is None
 
@@ -7,3 +10,4 @@ def test_connect_to_db():
   db_engine = db_connection_handler.get_engine()
 
   assert db_engine is not None
+  assert isinstance(db_engine, Engine)
