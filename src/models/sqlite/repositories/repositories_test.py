e0be424d3fcce74cf686e235ad948733a4371799
thiagohrcosta <thiagohrcosta86@gmail.com>
2026-02-26 21:57:21 -0300

    add people repository

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,7 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 # db_connection_handler.connect_to_db()
 
@@ -16,3 +17,20 @@ def test_delete_pet():
   name = "luke"
   repo = PetsRepository(db_connection_handler)
   repo.delete_pet(name)
+
+@pytest.mark.skip(reason="Database interaction test with database")
+def test_insert_person():
+  first_name = "FirstName"
+  last_name = "LastName"
+  age = 80
+  pet_id = 1
+
+  repo = PeopleRepository(db_connection_handler)
+  repo.insert_person(first_name, last_name, age, pet_id)
+
+@pytest.mark.skip(reason="Database interaction test with database")
+def test_get_person():
+  person_id = 1
+
+  repo = PeopleRepository(db_connection_handler)
+  repo.get_person(person_id)
